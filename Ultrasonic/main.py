f3709db6774fe17ca6c1d9fb8132188b354a0160
emilyelhy <49402353+emilyelhy@users.noreply.github.com>
2020-11-03 11:20:24 +0800

    Update main.py

--- Ultrasonic/main.py
+++ Ultrasonic/main.py
@@ -1,11 +1,14 @@
-from machine import UART, Pin, Signal
 import machine
 import utime
 
 class Ultrasonic:
     def __init__(self):
-        self.trig = Pin(17, Pin.OUT)
-        self.echo = Pin(16, Pin.IN)
+        # serial write
+        self.Serial2 = machine.UART(2)
+        self.Serial2.init(115200, bits=8, parity=None, stop=1)
+        # ultrasonic sensor
+        self.trig = machine.Pin(17, machine.Pin.OUT)
+        self.echo = machine.Pin(16, machine.Pin.IN)
         self.time = 0.0
         # below use for counting time interval
         self.nowTime = 0.0
@@ -29,21 +32,23 @@ class Ultrasonic:
 
     def loop(self):
         self.trig.value(1)
-        utime.sleep_ms(60)
+        utime.sleep_ms(50)
         self.trig.value(0)
-        if self.getCount(50):                 # run per 50ms
-            self.time = machine.time_pulse_us(self.echo, 1)
-        if self.time > 0:
+        self.time = machine.time_pulse_us(self.echo, 1)
+        if self.time > 0 and self.time < 26471:         # becoz sensor ranges from 20mm-4500mm
             self.distance = (self.time*0.34) / 2
         else:
             self.distance = 0.0
-        print("distance(in mm):", self.distance)
+        # print("distance(in mm):", self.distance)
+        self.distance = "{:.2f}".format(self.distance)
+        command = "U" + str(self.distance)
+        print(command)
+        self.Serial2.write(command)
         self.time = 0.0
         self.distance = 0.0
 
 if __name__ == "__main__":
-    led = Pin(2, Pin.OUT)
-    led.off()
+    # ultrasonic sensor ranges from 20mm to 4500mm
     print("Start receiving")
     while True:
         Ultrasonic().loop()

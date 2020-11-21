177560d8b6e7d56a8d6b20e28b5517cd0f0880fb
emilyelhy <1155108739@link.cuhk.edu.hk>
2020-11-21 11:14:19 -0800

    flex main done, structure unfixed

--- Flex/main.py
+++ Flex/main.py
@@ -1,9 +1,18 @@
 from machine import Pin, ADC
 from time import sleep
 
-test = ADC(Pin(36))
+test = ADC(Pin(39))
+min = 1000
+straight = 1500
+max = 4095
 
 while True:
     value = test.read()
+    if value < straight:
+        value = straight
+    elif value > max:
+        value = max
+    value = (value - straight) / (max - straight)
+    value = "{:.2f}".format(value)
     print("[F]", value)
     sleep(0.05)

07af9aba0da2f7480cf7460141170d817f30b301
emilyelhy <49402353+emilyelhy@users.noreply.github.com>
2020-12-01 10:49:16 +0800

    Added to 5 sensors

--- Flex/main.py
+++ Flex/main.py
@@ -7,31 +7,30 @@ class Flex:
         self.Serial2 = machine.UART(2)
         self.Serial2.init(115200, bits=8, parity=None, stop=1)
         # flex sensor
-        self.thumb = machine.ADC(machine.Pin(39))
-        # self.index = 
-        # self.middle = 
-        # self.ring = 
-        # self.little = 
+        self.thumb = machine.ADC(machine.Pin(32))
+        self.index = machine.ADC(machine.Pin(35))
+        self.middle = machine.ADC(machine.Pin(34))
+        self.ring = machine.ADC(machine.Pin(36))
+        self.little = machine.ADC(machine.Pin(39))
         # flex const
-        self.min = 1000
-        self.max = 4095
-        self.straight = 1500
+        self.max = [4095, 1420, 1420, 1350, 4095]
+        self.straight = [1700, 700, 700, 700, 1700]
         # flex value
         self.value = [0, 0, 0, 0, 0]
 
     def loop(self):
         self.value[0] = self.thumb.read()
-        # self.value[1] = self.index.read()
-        # self.value[2] = self.middle.read()
-        # self.value[3] = self.ring.read()
-        # self.value[4] = self.little.read()
-        self.value[1] = self.value[2] = self.value[3] = self.value[4] = 0
+        self.value[1] = self.index.read()
+        self.value[2] = self.middle.read()
+        self.value[3] = self.ring.read()
+        self.value[4] = self.little.read()
+        # self.value[0] = self.value[4] = 0
         for i in range(5):
-            if self.value[i] < self.straight:
-                self.value[i] = self.straight
-            elif self.value[i] >self.max:
-                self.value[i] = self.max
-            self.value[i] = (self.value[i] - self.straight) / (self.max - self.straight)
+            if self.value[i] < self.straight[i]:
+                self.value[i] = self.straight[i]
+            elif self.value[i] >self.max[i]:
+                self.value[i] = self.max[i]
+            self.value[i] = (self.value[i] - self.straight[i]) / (self.max[i] - self.straight[i])
             self.value[i] = "{:.2f}".format(self.value[i])
         command = "F" + str(self.value)
         print(command)
